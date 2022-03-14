2dd74caeb34dd511654b2b6f4f3d97a42eeb5e6b
BruceRheaume <94859282+BruceRheaume@users.noreply.github.com>
2022-03-14 10:02:49 -0400

    Add files via upload

--- R/GlobalProbe.R
+++ R/GlobalProbe.R
@@ -6,8 +6,9 @@
 #' @param lab The reference @meta.data slot on which to compare global parameters
 #' @return Global correlation analyses of the clusters provided in lab parameter
 #' @references  
-#' Rheaume, B. A., & Trakhtenberg, E. F. (2021). Self-learning optimization algorithm for integration 
-#' of scRNA-seq datasets improves the identification of resilient and susceptible retinal ganglion cell types. 
+#' Rheaume, B. A., & Trakhtenberg, E. F. (2022). Self-learning algorithm for denoising 
+#' and advancing the integration of scRNA-seq datasets improves the identification of resilient 
+#' and susceptible retinal ganglion cell types 
 #' \href{https://www.biorxiv.org/content/10.1101/2021.10.15.464552v2.abstract}{bioRxiv}.
 #' @export
 GlobalProbe <- function(dat, lab){
